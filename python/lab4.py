1c655398a67d2b1b57ad8f95aa044bbfdcf2dfb2
stoniques <nastoianov@gmail.com>
2019-11-30 22:39:20 +0200

    Lab 4 complete

--- python/lab4.py
+++ python/lab4.py
@@ -1,5 +1,4 @@
-matrix = [[1,4,5],
-         [-5,-12,8]]
+import numpy
 
 def generate_from_keyboard():
     rows = int(input("Rows: "))
@@ -14,15 +13,15 @@ def generate_from_keyboard():
 
     for i in range(rows):
         for j in range(columns):
-            print("entry in row ", i, "column", j)
+            print(f'Enter value for row {i}, column {j}: ')
             matrix[i][j] = int(input())
     print(matrix)
+    return(matrix)
 
 def generate_random_matrix():
-    from numpy import random as random
-    rows = random.randint(1,4)
-    columns = random.randint(1,4)
-    matrix = random.randint(10, size=(rows,columns)).tolist()
+    rows = numpy.random.randint(1,4)
+    columns = numpy.random.randint(1,4)
+    matrix = numpy.random.randint(10, size=(rows,columns)).tolist()
     return matrix
 
 def build_matrix():
@@ -31,19 +30,18 @@ def build_matrix():
     else:
         enter_by_user = False
 
-    print(enter_by_user)
-
     matrix = generate_from_keyboard() if enter_by_user else generate_random_matrix()
     return matrix
 
 def divisible_by_four(matrix):
     flattened = [y for x in matrix for y in x]
-    divisible_by_four = [x for x in flattened if x % 4 == 0]
+    divisible_by_four = [x for x in flattened if x % 4 == 0 and x != 0]
     return divisible_by_four
 
 try:
     matrix = build_matrix()
     divisible_by_four = divisible_by_four(matrix)
+    print(f'Matrix:\n {numpy.matrix(matrix)}')
     print(f'{len(divisible_by_four)} elements can be divided by 4: {divisible_by_four}')
     # print(divisible_by_four)
 except Exception as e:
